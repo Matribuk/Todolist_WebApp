949a4b7562116c92532e4f375b2d795172a01492
Matribuk <antonin.leprest@epitech.eu>
2023-06-16 19:25:16 +0200

    :sparkles: to do & list with delete option

--- src/lib/main.dart
+++ src/lib/main.dart
@@ -5,99 +5,26 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'My TodoList',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a blue toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme:
-            ColorScheme.fromSeed(seedColor: Color.fromARGB(255, 0, 238, 151)),
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: const Color.fromARGB(255, 238, 91, 0),
+        ),
         useMaterial3: true,
       ),
-      home: const HomePage(title: 'Flutter Home Page'),
+      home: const MyHomePage(title: ' Todo List '),
       debugShowCheckedModeBanner: false,
     );
   }
 }
 
-// Page d'accueil
-class HomePage extends StatelessWidget {
-  const HomePage({super.key, required this.title});
-  final String title;
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Page d\'accueil'),
-      ),
-      body: Center(
-        child: ElevatedButton(
-          child: const Text('Aller à la deuxième page'),
-          onPressed: () {
-            Navigator.push(
-              context,
-              MaterialPageRoute(
-                  builder: (context) =>
-                      const MyHomePage(title: 'Flutter Home Page')),
-            );
-          },
-        ),
-      ),
-    );
-  }
-}
-
-// Deuxième page
-class SecondPage extends StatelessWidget {
-  const SecondPage({super.key});
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Deuxième page'),
-      ),
-      body: Center(
-        child: ElevatedButton(
-          child: const Text('Retour à la page d\'accueil'),
-          onPressed: () {
-            Navigator.pop(context);
-          },
-        ),
-      ),
-    );
-  }
-}
-
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
   final String title;
 
   @override
@@ -105,12 +32,58 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-  String _text = '';
+  List<String> todoItems = [];
 
-  void _incrementCounter() {
+  void _createTask() {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        String newTask = '';
+
+        return AlertDialog(
+          title: const Text('Add Task'),
+          content: TextField(
+            autofocus: true,
+            onChanged: (value) {
+              newTask = value;
+            },
+            onSubmitted: (value) {
+              if (value.isNotEmpty) {
+                setState(() {
+                  todoItems.add(value);
+                });
+                Navigator.pop(context);
+              }
+            },
+            decoration: const InputDecoration(
+              hintText: 'Enter a task',
+            ),
+          ),
+          actions: [
+            TextButton(
+              onPressed: () => Navigator.pop(context),
+              child: const Text('Cancel'),
+            ),
+            TextButton(
+              onPressed: () {
+                if (newTask.isNotEmpty) {
+                  setState(() {
+                    todoItems.add(newTask);
+                  });
+                  Navigator.pop(context);
+                }
+              },
+              child: const Text('Add'),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  void _deleteTask(int index) {
     setState(() {
-      _counter++;
+      todoItems.removeAt(index);
     });
   }
 
@@ -121,49 +94,37 @@ class _MyHomePageState extends State<MyHomePage> {
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Text(widget.title),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'Texte saisi :',
-              style: TextStyle(fontSize: 20.0),
+      backgroundColor: const Color.fromARGB(255, 39, 40, 39),
+      body: ListView.builder(
+        itemCount: todoItems.length,
+        itemBuilder: (context, index) {
+          final task = todoItems[index];
+
+          return Dismissible(
+            key: Key(task),
+            onDismissed: (_) {
+              setState(() {
+                todoItems.removeAt(index);
+              });
+              ScaffoldMessenger.of(context).showSnackBar(
+                SnackBar(
+                  content: Text('Task "$task" deleted'),
+                  duration: const Duration(seconds: 2),
+                ),
+              );
+            },
+            child: ListTile(
+              title: Text(task),
+              textColor: const Color.fromARGB(255, 255, 255, 255),
+              trailing: const Icon(Icons.check_circle),
+              onTap: () => _deleteTask(index),
             ),
-            Text(
-              _text,
-              style:
-                  const TextStyle(fontSize: 24.0, fontWeight: FontWeight.bold),
-            ),
-            const SizedBox(height: 20.0),
-            TextField(
-              onChanged: (value) {
-                setState(() {
-                  _text = value;
-                });
-              },
-              decoration: const InputDecoration(
-                labelText: 'Saisissez votre texte',
-              ),
-            ),
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-            ElevatedButton(
-              child: const Text('Retour à la page d\'accueil'),
-              onPressed: () {
-                Navigator.pop(context);
-              },
-            ),
-          ],
-        ),
+          );
+        },
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
+        onPressed: _createTask,
+        tooltip: 'Add Task',
         child: const Icon(Icons.add),
       ),
     );
